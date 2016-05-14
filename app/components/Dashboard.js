3cc6dac14c2537ecf4f85fc580bd9dcb9067b972
rlatkdgus500 <rlatkdgus500@gmail.com>
2016-05-14 10:18:31 +0900

    Delete unused value and line

--- app/components/Dashboard.js
+++ app/components/Dashboard.js
@@ -11,23 +11,6 @@ import {
     NavigatorIOS
 } from 'react-native';
 
-// import RNChart from 'react-native-chart';
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: 'white',
-    },
-    chart: {
-        position: 'absolute',
-        top: 16,
-        left: 4,
-        bottom: 4,
-        right: 16,
-    }
-});
 export default class GoniDashboard extends Component {
 
     // constructor -> sett state
@@ -49,23 +32,3 @@ export default class GoniDashboard extends Component {
         );
     }
 }
-
-const chartData = [
-    {
-        name: 'BarChart',
-        type: 'bar',
-        color:'purple',
-        widthPercent: 0.6,
-        data: [43, 41, 21, 12, 53, 245, 21, 116, 12, 234, 535, 36],
-    },
-    {
-        name: 'LineChart',
-        color: 'gray',
-        lineWidth: 2,
-        highlightIndices: [1, 2],
-        highlightColor: 'blue',
-        showDataPoint: true,
-        data: [35, 43, 24, 25, 65, 87, 89, 66, 45, 16, 84, 12],
-    }
-];
-const xLabels = ['0','1','2','3','4','5','6','7','8','9','10','11'];
