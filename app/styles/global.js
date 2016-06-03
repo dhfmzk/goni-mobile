fc27b02d04aaf57e0ae7fb1339c3f54ea5124567
rlatkdgus500 <rlatkdgus500@gmail.com>
2016-06-03 17:16:50 +0900

    Add global stylesheet

---
+++ app/styles/global.js
@@ -0,0 +1,22 @@
+'use strict'
+
+import React from 'react';
+import {
+  StyleSheet,
+} from 'react-native';
+
+var gStyles = StyleSheet.create({
+    card: {
+        borderWidth: 1,
+        backgroundColor: '#fff',
+        borderColor: 'rgba(0,0,0,0.1)',
+        margin: 5,
+        padding: 5,
+        shadowColor: '#ccc',
+        shadowOffset: { width: 2, height: 2, },
+        shadowOpacity: 0.5,
+        shadowRadius: 3
+    },
+});
+
+module.exports = gStyles;
