9ff185d15a6b78b141c2c207d9735a9a6894ee73
rlatkdgus500 <rlatkdgus500@gmail.com>
2016-06-09 21:56:06 +0900

    fix: when dashboard data is data is null, exception handling

--- app/sections/Dashboard.js
+++ app/sections/Dashboard.js
@@ -26,15 +26,6 @@ import gStyles from '../styles/global'
 const GONIPLUS_ROOT_URI = 'https://dashboard.goniapm.io/api/goniplus/'
 const SUFFIX_CPU = '/overview/dashboard/cpu'
 
-var testData1 = [
-    {name: 'test1', primaryColor: '#2c5ae9', secondaryColor: 'white', values: [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ]}
-]
-var testData2 = [
-    {name: '200', primaryColor: '#7cd06b', secondaryColor: 'white', values: [68,73,71,62,89]},
-    {name: '401', primaryColor: '#ffda00', secondaryColor: 'white', values: [20,13,24,15, 6]},
-    {name: '500', primaryColor: '#ff7595', secondaryColor: 'white', values: [12,14, 5,23, 5]}
-]
-
 export default class DashboardSection extends Component {
 
     constructor(props) {
@@ -119,82 +110,108 @@ export default class DashboardSection extends Component {
             }
             if (request.status === 200) {
                 var responseJSON = JSON.parse(request.responseText);
-                this.setState({
-                    detail: this._processDetail(responseJSON),
-                    isActiveUserLoaded: true,
-                    isTransectionLoaded: true
-                })
+                this._processDetail(responseJSON)
             } else {
                 console.warn('error');
             }
         };
-        request.open('GET', GONIPLUS_ROOT_URI+this.props.projectKey+SUFFIX_CPU+'/'+'1465813200');
+        request.open('GET', GONIPLUS_ROOT_URI+this.props.projectKey+SUFFIX_CPU+'/'+_time.toString());
         request.setRequestHeader('Authorization', 'Bearer ' + token);
         request.send();
     }
 
     _processDetail(_data) {
+        console.log(_data);
+        if(!_data) { return null }
         var result = {}
         // process active user
         var tempObj = {}
+        var a = {}
         var l = []
-        tempObj['name'] = 'Active User Count'
-        tempObj['primaryColor'] = '#2c5ae9'
-        tempObj['secondaryColor'] = 'white'
-        tempObj['values'] = []
-        _data['user'].forEach((item) => {
-            tempObj['values'].push(item['value'])
-        })
-        result['user'] = {}
-        result['user']['graph'] = []
-        result['user']['graph'].push(tempObj)
+        var max = []
+        var checkT = false;
+        var checkU = false;
+        if(_data['user']) {
+            if(_data['user'].length > 0) {
+                // if(_data['user'] == 'undefined') { return null }
+                checkU = true
+                tempObj['name'] = 'Active User Count'
+                tempObj['primaryColor'] = '#2c5ae9'
+                tempObj['secondaryColor'] = 'white'
+                tempObj['values'] = []
+                _data['user'].forEach((item) => {
+                    tempObj['values'].push(item['value'])
+                })
+                result['user'] = {}
+                result['user']['graph'] = []
+                result['user']['graph'].push(tempObj)
 
-        tempObj = []
-        _data['user'].forEach((item) => {
-            tempObj.push(item['time'].match(/:[0-9]{2}:[0-9]{2}/)[0])
-        })
-        result['user']['categoryLabels'] = tempObj
-
-        // process Transection
-        tempObj = [
-            { name: '200', primaryColor: '#4c80f1', secondaryColor: 'white', values: []},
-            { name: '401', primaryColor: '#ffda00', secondaryColor: 'white', values: []},
-            { name: '500', primaryColor: '#ff7595', secondaryColor: 'white', values: []}
-        ]
-        _data['transaction'].sort((a, b) => {
-            if (a.mean > b.mean) { return -1; }
-            if (a.mean < b.mean) { return  1; }
-            else { return 0; }
-        })
-        _data['transaction'] = _data['transaction'].slice(0,5)
-        _data['transaction'].forEach((item) => {
-            l.push(item['path'])
-            tempObj[0]['values'].push(0)
-            tempObj[1]['values'].push(0)
-            tempObj[2]['values'].push(0)
-            _data['transactionStatus'].forEach((check) => {
-                if(check['path'] == item['path']) {
-                    if(check['status'] == '200') {
-                        tempObj[0]['values'].pop()
-                        tempObj[0]['values'].push(check['count'])
-                    }
-                    if(check['status'] == '401') {
-                        tempObj[1]['values'].pop()
-                        tempObj[1]['values'].push(check['count'])
-                    }
-
-                    if(check['status'] == '500') {
-                        tempObj[2]['values'].pop()
-                        tempObj[2]['values'].push(check['count'])
+                tempObj = []
+                _data['user'].forEach((item) => {
+                    tempObj.push(item['time'].match(/:[0-9]{2}:[0-9]{2}/)[0])
+                    max.push(item['value'])
+                })
+                for (const i of Array(15).fill(0).map((_, i) => i)) {
+                    if(i % 4 !== 0) {
+                        tempObj[i] = ''
+                    }else {
+                        tempObj[i] = tempObj[i].substr(1,5)
                     }
                 }
+                result['user']['categoryLabels'] = tempObj
+                result['user']['scale'] = { min: 0, max: Math.max(...max)>0? Math.max(...max) : 10, unit: Math.ceil(Math.max(...max)/10)>0? Math.ceil(Math.max(...max)/10) : 1 }
+            }
+        }
+        // process Transection
+        if(_data.length > 0) {
+            checkT = true
+            tempObj = [
+                { name: '200', primaryColor: '#4c80f1', secondaryColor: 'white', values: []},
+                { name: '401', primaryColor: '#ffda00', secondaryColor: 'white', values: []},
+                { name: '500', primaryColor: '#ff7595', secondaryColor: 'white', values: []}
+            ]
+            max = []
+            _data['transaction'].sort((a, b) => {
+                if (a.mean > b.mean) { return -1; }
+                if (a.mean < b.mean) { return  1; }
+                else { return 0; }
             })
-        })
-        result['transaction'] = {}
-        result['transaction']['data'] = tempObj
-        result['transaction']['categoryLabels'] = l
+            _data['transaction'] = _data['transaction'].slice(0,5)
+            _data['transaction'].forEach((item) => {
+                max.push(item['count'])
+                l.push(item['path'])
+                tempObj[0]['values'].push(0)
+                tempObj[1]['values'].push(0)
+                tempObj[2]['values'].push(0)
+                _data['transactionStatus'].forEach((check) => {
+                    if(check['path'] == item['path']) {
+                        if(check['status'] == '200') {
+                            tempObj[0]['values'].pop()
+                            tempObj[0]['values'].push(check['count'])
+                        }
+                        if(check['status'] == '401') {
+                            tempObj[1]['values'].pop()
+                            tempObj[1]['values'].push(check['count'])
+                        }
+
+                        if(check['status'] == '500') {
+                            tempObj[2]['values'].pop()
+                            tempObj[2]['values'].push(check['count'])
+                        }
+                    }
+                })
+            })
+            result['transaction'] = {}
+            result['transaction']['data'] = tempObj
+            result['transaction']['categoryLabels'] = l
+            result['transaction']['scale'] = { min: 0, max: Math.max(...max)>0? Math.max(...max) : 10, unit: Math.ceil(Math.max(...max)/10)>0? Math.ceil(Math.max(...max)/10) : 1 }
+        }
         console.log(result);
-        return result
+        this.setState({
+            detail: result,
+            isActiveUserLoaded: checkU,
+            isTransectionLoaded: checkT
+        })
     }
 
     render() {
@@ -210,13 +227,13 @@ export default class DashboardSection extends Component {
     _renderCPU() {
         if(this.state.isCPULoaded && this.state.firstAnimEnd) {
             return (
-                <Animatable.View animation="fadeInDown" easing="ease-out" style={[gStyles.card, {paddingBottom: 20}]}>
+                <Animatable.View animation="fadeInDown" easing="ease-out" style={[gStyles.card, {paddingBottom: 20, height: 300}]}>
                     <View style={{margin:10, flexDirection: 'row', alignItems: 'center'}}>
                         <Text style={{fontSize: 22, color: '#4d5256'}}>System Status</Text>
                         <Text style={{marginLeft: 8, borderRadius: 2, borderColor: '#2c5ae9', borderWidth: 0.8, padding: 2, fontSize: 8, width: 35, textAlign: 'center', color: '#2c5ae9'}}>CPU</Text>
                     </View>
                     <View style={gStyles.decoBar}></View>
-                    <Animatable.View animation="bounceIn" easing="ease-out">
+                    <Animatable.View animation="bounceIn" easing="ease-out" style={{marginTop: 15}}>
                         <HeatmapChart
                             colorStream={['#E1E4E6', '#87b1f3', '#6b9df3', '#5188f2', '#3b72ef', '#2c5ae9']}
                             dataSet={this.state.CPUData}
@@ -227,7 +244,7 @@ export default class DashboardSection extends Component {
             );
         }else {
             return(
-                <Animatable.View animation="fadeInDown" easing="ease-out" onAnimationEnd={() => this.setState({firstAnimEnd: true})} style={gStyles.card}>
+                <Animatable.View animation="fadeInDown" easing="ease-out" style={[gStyles.card, {paddingBottom: 20, height: 300}]}>
                     <View style={{margin:10, flexDirection: 'row', alignItems: 'center'}}>
                         <Text style={{fontSize: 22, color: '#4d5256'}}>System Status</Text>
                         <Text style={{marginLeft: 8, borderRadius: 2, borderColor: '#2c5ae9', borderWidth: 0.8, padding: 2, fontSize: 8, width: 35, textAlign: 'center', color: '#2c5ae9'}}>CPU</Text>
@@ -249,7 +266,7 @@ export default class DashboardSection extends Component {
     _renderActiveUser() {
         if(this.state.isActiveUserLoaded && this.state.firstAnimEnd) {
             return (
-                <Animatable.View animation="fadeInDown" easing="ease-out" style={gStyles.card}>
+                <Animatable.View animation="fadeInDown" easing="ease-out" style={[gStyles.card, {height: 250}]}>
                     <View style={{margin:10}}>
                         <Text style={{fontSize: 22, color: '#4d5256'}}>Active User</Text>
                     </View>
@@ -264,7 +281,7 @@ export default class DashboardSection extends Component {
                             valueLabelStyle={{color: '#a9afb3'}}
                             categoryLabelStyle={{fontSize: 8, color: '#a9afb3'}}
                             categoryAxisMode={'point'}
-                            valueScale={{min: 0, max: 30, unit: 5}}
+                            valueScale={this.state.detail['user']['scale']}
                             categoryLabels={this.state.detail['user']['categoryLabels']}>
                             <LineChart
                                 lineWidth={1}
@@ -272,7 +289,7 @@ export default class DashboardSection extends Component {
                                 pointRadius={2}
                                 categoryAxisMode={'range'}
                                 datasets={this.state.detail['user']['graph']}
-                                valueScale={{min: 0, max: 13, unit: 1}}
+                                valueScale={this.state.detail['user']['scale']}
                             />
                         </Axes>
                     </Animatable.View>
@@ -280,12 +297,12 @@ export default class DashboardSection extends Component {
             );
         }else {
             return (
-                <Animatable.View animation="fadeInDown" easing="ease-out" style={gStyles.card}>
+                <Animatable.View animation="fadeInDown" easing="ease-out" delay={300} style={[gStyles.card, {height: 250}]}>
                     <View style={{margin:10}}>
                         <Text style={{fontSize: 22, color: '#4d5256'}}>Active User</Text>
                     </View>
                     <View style={gStyles.decoBar}></View>
-                    <View style={{flexDirection: 'column', alignItems: 'center', height: 250}}>
+                    <View style={{flex: 1, flexDirection: 'column', alignItems: 'center'}}>
                         <View style={{flex:1, flexDirection: 'row', alignItems: 'center'}}>
                             <Text style={{fontSize: 20}}>
                                 Status Box를 선택해주세요
@@ -309,27 +326,45 @@ export default class DashboardSection extends Component {
                         <View>
                         </View>
                     </View>
+                    <Animatable.View animation="bounceIn" easing="ease-out" style={{flex: 1, height: 50}}>
+                        <View style={{flexDirection: 'column', alignItems: 'center', height: 30}}>
+                            <View style={{flex:1, flexDirection: 'row', alignItems: 'center'}}>
+                                <View style={{marginLeft: 5, marginRight: 5, flexDirection: 'row'}}>
+                                    <Text style={{color: '#a9afb3', fontSize: 10}}>200</Text>
+                                    <View style={{marginLeft: 5, marginRight: 5, width:30, backgroundColor: '#4c80f1', borderRadius: 2}}></View>
+                                </View>
+                                <View style={{marginLeft: 5, marginRight: 5, flexDirection: 'row'}}>
+                                    <Text style={{color: '#a9afb3', fontSize: 10}}>401</Text>
+                                    <View style={{marginLeft: 5, marginRight: 5, width:30, backgroundColor: '#ffda00', borderRadius: 2}}></View>
+                                </View>
+                                <View style={{marginLeft: 5, marginRight: 5, flexDirection: 'row'}}>
+                                    <Text style={{color: '#a9afb3', fontSize: 10}}>500</Text>
+                                    <View style={{marginLeft: 5, marginRight: 5, width:30, backgroundColor: '#ff7595', borderRadius: 2}}></View>
+                                </View>
+                            </View>
+                        </View>
+                    </Animatable.View>
                     <Animatable.View animation="bounceIn" easing="ease-out">
                         <Axes
-                            style={{height: 300, paddingRight: 10}}
+                            style={{height: 200, paddingRight: 10}}
                             showCategoryTicks={false}
                             showCategoryGridlines={false}
                             showValueGridlines={false}
                             valueAxisStyle={{axisLineWidth: 1, axisLineColor: '#a9afb3', gridlineColor: '#a9afb3'}}
                             categoryAxisStyle={{axisLineWidth: 1, axisLineColor: '#a9afb3', gridlineColor: '#ced3d6'}}
                             valueLabelStyle={{color: '#a9afb3'}}
-                            categoryLabelStyle={{color: '#a9afb3'}}
-                            valueScale={{min: 0, max: 100, unit: 10}}
+                            categoryLabelStyle={{color: '#a9afb3', fontSize: 8}}
+                            valueScale={this.state.detail['transaction']['scale']}
                             categoryLabels={this.state.detail['transaction']['categoryLabels']}
                             orientation='horizontal'>
                             <BarChart
-                                style={{height: 300}}
+                                style={{height: 200}}
                                 spacing={1}
                                 clusterSpacing={1}
                                 barBorderWidth={1}
                                 displayMode={'stacked'}
                                 datasets={this.state.detail['transaction']['data']}
-                                valueScale={{min: 0, max: 100, unit: 10}}
+                                valueScale={this.state.detail['transaction']['scale']}
                             />
                         </Axes>
                     </Animatable.View>
@@ -337,7 +372,7 @@ export default class DashboardSection extends Component {
             );
         }else {
             return (
-                <Animatable.View animation="fadeInDown" easing="ease-out" style={gStyles.card}>
+                <Animatable.View animation="fadeInDown" easing="ease-out" onAnimationEnd={() => this.setState({firstAnimEnd: true})} delay={600} style={gStyles.card}>
                     <View style={{margin:10}}>
                         <Text style={{fontSize: 22, color: '#4d5256'}}>Top 5 Transection</Text>
                     </View>
@@ -354,20 +389,3 @@ export default class DashboardSection extends Component {
         }
     }
 }
-
-var styles = StyleSheet.create({
-    button: {
-        flex: 0.5,
-        height: 30,
-        borderColor: '#5f6466',
-        borderRadius: 4,
-        padding: 10,
-        margin:10
-    },
-    red: {
-        borderColor: '#ff7595'
-    },
-    blue: {
-        borderColor: '#4c80f1'
-    }
-})
